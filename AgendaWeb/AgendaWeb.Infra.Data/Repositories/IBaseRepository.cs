0f149613d169f093e61dbf225d68046419328c6b
Karmaicom <karmaicom@gmail.com>
2022-03-30 12:14:27 -0300

    Telas Razor: Cadsatro da Tarefa e Consulta da Tarefa; Configuração da connectionString do banco de dados BDAgendaWeb; Criação da projeto (biblioteca de classes) AgendaWeb.Infra.Data; Criação da entidade Tarefa; Criação da interface IBaseRepository;

---
+++ AgendaWeb/AgendaWeb.Infra.Data/Repositories/IBaseRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaWeb.Infra.Data.Repositories
+{
+    public interface IBaseRepository<TEntity, TKey>
+    {
+
+        void Create(TEntity obj);
+
+        void Update(TEntity obj);
+
+        void Delete(TKey key);
+
+        List<TEntity> FindAll();
+
+        TEntity FindById(TKey key);
+
+    }
+}
