d632775ff763d25a21fc95b190a3972a823360ab
Karmaicom <karmaicom@gmail.com>
2022-03-30 10:59:24 -0300

    Criação do Controller Tarefa; Criação da tela de Cadastro de Tarefas;

---
+++ AgendaWeb/AgendaWeb.Presentation/Controllers/TarefaController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AgendaWeb.Presentation.Controllers
+{
+    public class TarefaController : Controller
+    {
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public IActionResult Consulta()
+        {
+            return View();
+        }
+    }
+}
