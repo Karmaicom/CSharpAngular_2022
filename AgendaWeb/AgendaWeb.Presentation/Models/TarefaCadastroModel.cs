9495f241fffe143546e0ce5c3df5284fab2c1507
Karmaicom <karmaicom@gmail.com>
2022-03-31 12:41:38 -0300

    Criação da classe Model/TarefaCadastroModel para ser usado na tela Tarefa/Cadastro; Criação da tela razor Cadastro.cshtml para preencher o Model TarefaCadastroModel;

---
+++ AgendaWeb/AgendaWeb.Presentation/Models/TarefaCadastroModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgendaWeb.Presentation.Models
+{
+    public class TarefaCadastroModel
+    {
+        [Required(ErrorMessage = "Informe o nome da tarefa.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Informe o data da tarefa.")]
+        public string Data { get; set; }
+
+        [Required(ErrorMessage = "Informe a hora da tarefa.")]
+        public string Hora { get; set; }
+
+        [Required(ErrorMessage = "Informe a descrição da tarefa.")]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Informe a prioridade da tarefa.")]
+        public string Prioridade { get; set; }
+
+    }
+}
--- AgendaWeb/AgendaWeb.Presentation/Program.cs
+++ AgendaWeb/AgendaWeb.Presentation/Program.cs
@@ -1,3 +1,5 @@
+using AgendaWeb.Infra.Data.Repositories;
+
 namespace AgendaWeb.Presentation
 {
     public class Program
@@ -6,12 +8,17 @@ namespace AgendaWeb.Presentation
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            var connectionString = builder.Configuration.GetConnectionString("BDAgendaWeb");
+
+            builder.Services.AddTransient<ITarefaRepository>(
+                map => new TarefaRepository(connectionString)
+            );
+
+
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Error");
