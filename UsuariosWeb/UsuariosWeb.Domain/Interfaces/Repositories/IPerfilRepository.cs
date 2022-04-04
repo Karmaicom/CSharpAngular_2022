5e71e6926a42a444ef37011132e786fa66567f4f
Karmaicom <karmaicom@gmail.com>
2022-04-04 19:09:46 -0300

    Criação das Entidades do Domínio; Criação das Interfaces de Repositório no Domínio; Criação do projeto UsuariosWeb.Infra.Data; Configuração da ConnectionString do banco de dados;

---
+++ UsuariosWeb/UsuariosWeb.Domain/Interfaces/Repositories/IPerfilRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UsuariosWeb.Domain.Entities;
+
+namespace UsuariosWeb.Domain.Interfaces.Repositories
+{
+    public interface IPerfilRepository : IBaseRepository<Perfil>
+    {
+    }
+}
---
+++ UsuariosWeb/UsuariosWeb.Domain/Interfaces/Repositories/IUsuarioRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UsuariosWeb.Domain.Entities;
+
+namespace UsuariosWeb.Domain.Interfaces.Repositories
+{
+    public interface IUsuarioRepository : IBaseRepository<Usuario>
+    {
+
+        Usuario Obter(string email);
+        Usuario Obter(string email, string senha);
+
+    }
+}
