15ab1833892c2571a66c3bf989e55fcf99f79d66
Karmaicom <karmaicom@gmail.com>
2022-04-05 10:58:44 -0300

    Inclusão do bootstrap no projeto; Criação da sobrecarga do método Login, no controller AccountController, para requisições [HttpPost] e como parâmetro a classe AccountLoginModel; Criação e implementação da tela de Login; Validão dos campos da tela de Login;

--- UsuariosWeb/UsuariosWeb.Presentation/Controllers/AccountController.cs
+++ UsuariosWeb/UsuariosWeb.Presentation/Controllers/AccountController.cs
@@ -1,10 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using UsuariosWeb.Presentation.Models;
 
 namespace UsuariosWeb.Presentation.Controllers
 {
     public class AccountController : Controller
     {
+
+        //abrir a página /Account/Login
         public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(AccountLoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+
+            return View();
+        }
+
+        //abrir a página /Account/Register
+        public IActionResult Register()
         {
             return View();
         }
---
+++ UsuariosWeb/UsuariosWeb.Presentation/Models/AccountLoginModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsuariosWeb.Presentation.Models
+{
+    public class AccountLoginModel
+    {
+
+        [Required(ErrorMessage = "Informe seu e-mail.")]
+        [EmailAddress(ErrorMessage = "Informe um e-mail válido.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Informe sua senha.")]
+        [MinLength(6, ErrorMessage = "Mínimo de {1} caracteres.")]
+        [MaxLength(20, ErrorMessage = "Máximo de {1} caracteres.")]
+        public string Senha { get; set; }
+
+    }
+}
