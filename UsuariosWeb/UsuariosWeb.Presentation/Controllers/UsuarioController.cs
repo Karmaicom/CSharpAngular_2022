9d9a87296b1482daf378deb3aa3977d2f75b0c0d
Karmaicom <karmaicom@gmail.com>
2022-04-07 09:12:03 -0300

    Criação do controller UsuarioController e das páginas vazias: Usuario/Cadastro, usuario/Consulta, Usuario/MinhaConta e Usuario/Relatorio;

---
+++ UsuariosWeb/UsuariosWeb.Presentation/Controllers/UsuarioController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UsuariosWeb.Presentation.Controllers
+{
+    [Authorize]
+    public class UsuarioController : Controller
+    {
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public IActionResult Consulta()
+        {
+            return View();
+        }
+
+        public IActionResult Relatorio()
+        {
+            return View();
+        }
+
+        public IActionResult MinhaConta()
+        {
+            return View();
+        }
+    }
+}
