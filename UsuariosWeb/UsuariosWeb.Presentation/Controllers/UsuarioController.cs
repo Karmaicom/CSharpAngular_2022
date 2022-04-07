9e670010ce50df613964505fee990df016073646
Karmaicom <karmaicom@gmail.com>
2022-04-07 11:26:42 -0300

    Criação da classe UsuarioMinhaContaModel; Implementada a Injeçãod e Dependência IUsuarioDomainService, na classe UsuarioController; Implementação da págiuna MinhaConta.cshtml;

--- UsuariosWeb/UsuariosWeb.Presentation/Controllers/UsuarioController.cs
+++ UsuariosWeb/UsuariosWeb.Presentation/Controllers/UsuarioController.cs
@@ -1,11 +1,21 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using UsuariosWeb.Domain.Interfaces.Services;
+using UsuariosWeb.Presentation.Models;
 
 namespace UsuariosWeb.Presentation.Controllers
 {
     [Authorize]
     public class UsuarioController : Controller
     {
+
+        private readonly IUsuarioDomainService _usuarioDomainService;
+
+        public UsuarioController(IUsuarioDomainService usuarioDomainService)
+        {
+            _usuarioDomainService = usuarioDomainService;
+        }
+
         public IActionResult Cadastro()
         {
             return View();
@@ -23,7 +33,23 @@ namespace UsuariosWeb.Presentation.Controllers
 
         public IActionResult MinhaConta()
         {
-            return View();
+            var model = new UsuarioMinhaContaModel();
+
+            try
+            {
+                var usuario = _usuarioDomainService.ObterUsuario(User.Identity.Name);
+            
+                model.Nome = usuario.Nome;
+                model.Email = usuario.Email;
+                model.DataCadastro = usuario.DataCadastro;
+                model.Perfil = usuario.Perfil.Nome.ToUpper();
+            }
+            catch (Exception e)
+            {
+                TempData["MensagemErro"] = e.Message;
+            }
+
+            return View(model);
         }
     }
 }
---
+++ UsuariosWeb/UsuariosWeb.Presentation/Models/UsuarioMinhaContaModel.cs
@@ -0,0 +1,10 @@
+﻿namespace UsuariosWeb.Presentation.Models
+{
+    public class UsuarioMinhaContaModel
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public string Perfil { get; set; }
+    }
+}
