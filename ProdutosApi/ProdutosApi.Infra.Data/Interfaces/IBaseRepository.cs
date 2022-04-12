30ac90b811449151199aca7b6a5641d9d833b98c
Karmaicom <karmaicom@gmail.com>
2022-04-12 19:02:37 -0300

    Criação da interface repository IBaseRepository com métodos default para CRUD;

---
+++ ProdutosApi/ProdutosApi.Infra.Data/Interfaces/IBaseRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProdutosApi.Infra.Data.Interfaces
+{
+    public interface IBaseRepository<TEntity, TKey>
+        where TEntity : class
+    {
+
+        void Inserir(TEntity obj);
+        void Excluir(TEntity obj);
+        void Alterar(TEntity obj);
+        List<TEntity> Consultar();
+        TEntity ObterPorId(Guid id);
+
+    }
+}
